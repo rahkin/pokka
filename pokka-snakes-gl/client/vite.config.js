cdded8b7f1a42603c7a951ad73dab03c2e74e882
rahkin <rahkinda206@gmail.com>
2025-04-04 18:12:56 +0800

    fix: update vite config to handle audio files correctly

--- pokka-snakes-gl/client/vite.config.js
+++ pokka-snakes-gl/client/vite.config.js
@@ -22,9 +22,14 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html')
             },
             output: {
-                assetFileNames: 'assets/[name]-[hash][extname]',
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name.endsWith('.mp3')) {
+                        return 'assets/audio/[name][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
                 chunkFileNames: 'assets/[name]-[hash].js',
-                entryFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js'
             }
         }
     }
